31aae22b940c20e2d3930e3c1a938ad64ff56b3d
Parvesh Kaushal <36725011+parvesh6300@users.noreply.github.com>
2019-10-04 10:58:01 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// This file is seen only by Babel 7, not Babel 6.
+// There are other semantic differences: see
+//   https://babeljs.io/docs/en/config-files
+
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset',
+  ],
+};
